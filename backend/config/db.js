376485c9a0102a805f1f93a084851487bf70e69e
ArjunSamuderaAhliFikri <arjunsaf717@gmail.com>
2025-03-26 12:45:24 +0700

    refactoring code

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
-const uri =
-  "mongodb+srv://dbArjun:dbArjun27@junbase.8anzo.mongodb.net/Admin_Smawa?appName=JunBase";
+require("dotenv").config();
 
 const clientOptions = {
   serverApi: {
@@ -14,7 +13,7 @@ const clientOptions = {
 async function connectDB() {
   try {
     // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
-    await mongoose.connect(uri, clientOptions);
+    await mongoose.connect(process.env.DATABASE_KEY, clientOptions);
     await mongoose.connection.db.admin().command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
